8f2e332690c9940ac551dd0812164929c1a0f6b8
T.vijayalakshmi <52314223+vijayalakshmi58@users.noreply.github.com>
2019-07-14 22:14:27 -0700

    Update and rename README.md to pro1.py

---
+++ pro1.py
@@ -0,0 +1,20 @@
+# pro1.py
+v=int(input())
+c=[]
+for _ in range(v):
+ str.append(input())
+v,g,flag='',0,False
+for j in c[0]:
+    for i in c[1:]:
+        if len[i]==g:
+            flag=True
+            break
+        if i(g) !=j:
+            break
+        else:
+              v += i
+        if flag:
+            break
+        g+= 1
+        print(v)
+        
